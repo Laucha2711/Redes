0f5c5a00aff0a86ac5a23f653757e0bd7f67ef8b
Jazdaax <justoghioldi23@gmail.com>
2024-12-03 16:21:04 -0300

    funca el server

--- Assets/Main/Scripts/Server/GameManagers.cs
+++ Assets/Main/Scripts/Server/GameManagers.cs
@@ -3,22 +3,70 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using TMPro;
+using Photon.Realtime;
 
 public class GameManagers : MonoBehaviourPunCallbacks
 {
     public TMP_Text textIndicator;
+    public GameObject btnConect;
+
 
     private void Start()
     {
-        if (!PhotonNetwork.IsConnected);
+        btnConect.SetActive(false);
+    }
+
+
+    public void ConectPhoton()
+    {
+        if (!PhotonNetwork.IsConnected)
         PhotonNetwork.ConnectUsingSettings();
     }
 
+    public void CreatePlayer(string namePlayer)
+    {
+        PhotonNetwork.NickName = namePlayer;
+    }
+
+
+    // conexion de internet
     public override void OnConnected()
     {
         base.OnConnected();
         Debug.Log("conectados a photon");
         textIndicator.text = "conectados correctamente";
+    }
+
+    //verificamos la conexion al servidor de photon
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        textIndicator.text = "Bienvenido " + PhotonNetwork.NickName;
+        btnConect.SetActive(true);
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        base.OnDisconnected(cause);
+    }
+
+    public void CreateRoom()
+    {
+        string user = PhotonNetwork.NickName;
+        string nameRoom = "sala 1";
+
+        RoomOptions optionRoom = new RoomOptions();
+        optionRoom.IsVisible = true;
+        optionRoom.MaxPlayers = 2;
+        optionRoom.PublishUserId = true;
+
+        PhotonNetwork.JoinOrCreateRoom(nameRoom, optionRoom,TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        Debug.Log("estamos conectados a la sala " +  PhotonNetwork.CurrentRoom.Name + " bienvenido " + PhotonNetwork.NickName);
     }
 
 }
---
+++ Assets/Main/Scripts/Server/WindowHandler.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowHandler : MonoBehaviour
+{
+    public GameObject[] windows;
+
+
+    public void EnbleWindow(int idWindow)
+    {
+        windows[idWindow].SetActive(true);
+
+        for (int i = 0; i < windows.Length; i++) 
+        {
+            if (idWindow != i)
+            {
+                windows[i].SetActive(false);
+            }
+        }
+    }
+
+}
